980a90d8ba8b98d976e62eef7688ab87463ab44f
svsevolodl <v.l.sokolov858@gmail.com>
2021-10-20 19:40:31 +0300

    вывод в консоль

--- src/main/java/MyFirstProject.java
+++ src/main/java/MyFirstProject.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MyFirstProject {
+public class MyFirstProject {
+
+
+    public static void main(String [] args) {
+        for (int i = 1; i <= 40; i++){
+            if(i >= 20 && i <= 40){
+                System.out.println("Целое число "+ i);
+            }
+        }
+
+
+
+
+    }
 }
