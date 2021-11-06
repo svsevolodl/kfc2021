79be80a35bff8b4e9902b48af210ffbfd1d32c54
svsevolodl <v.l.sokolov858@gmail.com>
2021-11-06 13:01:04 +0300

    добавление 2 варианта

--- src/main/java/MyFirstProject.java
+++ src/main/java/MyFirstProject.java
@@ -3,7 +3,7 @@ public class MyFirstProject {
 
     public static void main(String [] args) {
         for (int i = 1; i <= 40; i++){
-            if(i >= 20 && i <= 40){
+            if (i >= 20 && i <= 40){
                 System.out.println("Целое число "+ i);
             }
         }
