e1ecf628a8a2fd5f3e12aee1d862a9e0136b98ce
svleon <vsevolod.sokolov@rentateam.ru>
2022-02-02 00:09:26 +0300

    testPerd

---
+++ src/main/java/Lesson_2/test.java
@@ -0,0 +1,17 @@
+package Lesson_2;
+import java.util.Scanner;
+
+public class test {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            //String text = sc.nextLine();
+            //int sim = sc.nextInt();
+            //char word = text.charAt(sim-1);
+            //System.out.println(word);
+            System.out.println(sc.nextLine().charAt(sc.nextInt()-1));
+            //put your code here
+        }
+    }
+
+
