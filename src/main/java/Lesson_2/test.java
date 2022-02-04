e548a3fe9f94154a79af43bf84bc96d0f2fe2678
svleon <vsevolod.sokolov@rentateam.ru>
2022-02-04 09:35:29 +0300

    testPerd

--- src/main/java/Lesson_2/test.java
+++ src/main/java/Lesson_2/test.java
@@ -1,17 +1,31 @@
 package Lesson_2;
 import java.util.Scanner;
 
-public class test {
+class Otrezki {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt(), b1 = sc.nextInt();
+        int a2 = sc.nextInt(), b2 = sc.nextInt();
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            //String text = sc.nextLine();
-            //int sim = sc.nextInt();
-            //char word = text.charAt(sim-1);
-            //System.out.println(word);
-            System.out.println(sc.nextLine().charAt(sc.nextInt()-1));
-            //put your code here
+        if ((a2-b1)*(b2-a1)<=0) {
+            if (a2==b1) {
+                System.out.print(a2);
+            }else if (b2==a1) {
+                System.out.print(b2);
+            }
+        }else if (a1>=a2 && b1<=b2){
+            System.out.print(a1+" "+b1);
+        }else if (a2>=a1 && b2<=b1) {
+            System.out.print(a2+" "+b2);
+        }else if (a1<a2 && a2<b1 && b1<b2) {
+            System.out.print(a2+" "+b1);
+        }else if (a2<a1 && a1<b2 && b2<b1) {
+            System.out.print(a1+" "+b2);
+        }else if (a2>b1) {
+            System.out.print("Пересечения нет");
         }
+
     }
 
 
+}
